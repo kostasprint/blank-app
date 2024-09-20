bafe5ba39a22fd2f3365d47d813d1a7c7d0f55ea
Costantino <kostasprint@users.noreply.github.com>
2024-09-20 11:41:45 +0000

    ciao

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ import os
 from transformers import AutoTokenizer
 
 # App title
-st.set_page_config(page_title="Streamlit Replicate Chatbot", page_icon="💬")
+st.set_page_config(page_title="WeMake Streamlit Replicate Chatbot", page_icon="💬")
 
 # Replicate Credentials
 with st.sidebar:
-    st.title('💬 Streamlit Replicate Chatbot')
+    st.title('💬 WeMake Streamlit Replicate Chatbot')
     st.write('Create chatbots using various LLM models hosted at [Replicate](https://replicate.com/).')
     if 'REPLICATE_API_TOKEN' in st.secrets:
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
@@ -20,17 +20,22 @@ with st.sidebar:
     os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
     st.subheader("Models and parameters")
-    model = st.selectbox("Select a model",("meta/meta-llama-3-70b-instruct", "mistralai/mistral-7b-instruct-v0.2", "google-deepmind/gemma-2b-it"), key="model")
+    model = st.selectbox("Select a model",("meta/meta-llama-3-70b-instruct","meta/meta-llama-3.1-405b-instruct",), key="model")
     if model == "google-deepmind/gemma-2b-it":
         model = "google-deepmind/gemma-2b-it:dff94eaf770e1fc211e425a50b51baa8e4cac6c39ef074681f9e39d778773626"
     
+    safe = st.sidebar.checkbox("Safe")
+
     temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.7, step=0.01, help="Randomness of generated output")
     if temperature >= 1:
-        st.warning('Values exceeding 1 produces more creative and random output as well as increased likelihood of hallucination.')
+         st.warning('Values exceeding 1 produces more creative and random output as well as increased likelihood of hallucination.')
     if temperature < 0.1:
-        st.warning('Values approaching 0 produces deterministic output. Recommended starting value is 0.7')
+         st.warning('Values approaching 0 produces deterministic output. Recommended starting value is 0.7')
     
     top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01, help="Top p percentage of most likely tokens for output generation")
+
+    
+
 
 # Store LLM-generated responses
 if "messages" not in st.session_state.keys():
@@ -59,6 +64,14 @@ def get_num_tokens(prompt):
     tokens = tokenizer.tokenize(prompt)
     return len(tokens)
 
+
+
+if safe:
+    safer = "Reply to me in Italian if I'm speaking in Italian. VERY IMPORTANT: Be safeguarded. I'm a kid, tell me firmly if I'm acting inappropriately and in case don't reply."
+else:
+    safer = " "
+    
+
 # Function for generating model response
 def generate_response():
     prompt = []
@@ -68,7 +81,7 @@ def generate_response():
         else:
             prompt.append("<|im_start|>assistant\n" + dict_message["content"] + "<|im_end|>")
     
-    prompt.append("<|im_start|>assistant")
+    prompt.append("<|im_start|>assistant" + safer + "<|im_end|>assistant")
     prompt.append("")
     prompt_str = "\n".join(prompt)
     
